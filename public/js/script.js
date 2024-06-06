cdeb692e84de56da4c19215503bafcb0f5adb2a5
Elizabeth Catherine Odei <bettylizzie25@gmail.com>
2024-06-06 11:43:39 +0000

    Math in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -31,7 +31,7 @@ const bottle1 = {
     color: 'yellow'
   }
 
-  
+
   
   const bottle2 = {
     size: 'small',
@@ -60,3 +60,36 @@ const bottle1 = {
   for (let i = 0; i <= 5;  i+=1) {
     console.log('We did it!', i);
   }
+
+  // Functions
+// Defining a function
+function login(username, password){
+  // Validate username and password
+  if (!username || !password) {
+    return 'Username or password not provided';
+  }
+  // Verify username and password
+if (username == 'Tilopsy'&& password == '1234') {  return 'User is logged in';
+} else {
+  return 'Invalid username or password';
+}
+}
+// Invoking a function
+login('Tilopsy');
+
+// Basic Arithmetic Operations
+11 + 12;
+1 + 0.5;
+43 - 12;
+45 / 7;
+5 * 3;
+14 % 3;
+5 * 3;
+
+Math.floor(45 / 24);
+45 % 24
+Math.random() * 1000;
+Math.max(34, 32, 37);
+2 * (3 + 4) - 5 / 2;
+
+
